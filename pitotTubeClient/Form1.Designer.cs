6c6fad343126c912e700ae56cc9231e3de3c61c8
TORICA <torica@ed.tus.ac.jp>
2017-03-24 18:35:47 +0900

    COMとボーレートを指定してLabelに風速を表示

--- pitotTubeClient/Form1.Designer.cs
+++ pitotTubeClient/Form1.Designer.cs
@@ -29,11 +29,121 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(45, 6);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.DropDown += new System.EventHandler(this.comboBox1_DropDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "COM:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "BR:";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(45, 38);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 20);
+            this.comboBox2.TabIndex = 3;
+            this.comboBox2.DropDown += new System.EventHandler(this.comboBox2_DropDown);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(172, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.WindowText;
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Window;
+            this.label3.Location = new System.Drawing.Point(9, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(121, 50);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "00.00";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.SystemColors.WindowText;
+            this.label4.Font = new System.Drawing.Font("MS UI Gothic", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label4.ForeColor = System.Drawing.SystemColors.Window;
+            this.label4.Location = new System.Drawing.Point(136, 74);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 50);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "[m/s]";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(258, 133);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "PitotTubeClient";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- pitotTubeClient/Form1.cs
+++ pitotTubeClient/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,116 @@ namespace pitotTubeClient
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void comboBox1_DropDown(object sender, EventArgs e)
+        {    
+            // すべてのシリアル・ポート名を取得する
+            string[] ports = SerialPort.GetPortNames();
+
+            // シリアルポートを毎回取得して表示するために表示の度にリストをクリアする
+            comboBox1.Items.Clear();
+
+            foreach (string port in ports)
+            {
+                // 取得したシリアル・ポート名を出力する
+                comboBox1.Items.Add(port);
+            }
+
+        }
+
+        private void comboBox2_DropDown(object sender, EventArgs e)
+        {
+            // ボーレートを毎回取得して表示するために表示の度にリストをクリアする
+            comboBox2.Items.Clear();
+
+            // ボーレートを出力する
+            comboBox2.Items.Add("115200"); //デフォルトなのでこれを最初にもってくる
+
+            comboBox2.Items.Add("9600");
+            comboBox2.Items.Add("14400");
+            comboBox2.Items.Add("19200");
+            comboBox2.Items.Add("28800");
+            comboBox2.Items.Add("38400");
+            comboBox2.Items.Add("57600");
+            comboBox2.Items.Add("76800");
+            comboBox2.Items.Add("153600");
+            comboBox2.Items.Add("230400");
+            comboBox2.Items.Add("460800");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //comboBox1とcomboBox2に設定があればシリアルポートを開く
+            if (comboBox1.Text != "" && comboBox2.Text != "")
+            {
+                serialPort1.PortName = comboBox1.Text;
+
+                serialPort1.BaudRate = Convert.ToInt32(comboBox2.Text);
+
+                if (serialPort1.IsOpen == true)
+                {
+                    serialPort1.Close();
+                }
+
+                if (serialPort1.IsOpen == false)
+                {
+                    serialPort1.Open();
+
+                    if (serialPort1.IsOpen == true)
+                    {
+                        MessageBox.Show("Open Success\n" + serialPort1.PortName.ToString() + " " + serialPort1.BaudRate.ToString());
+                    }
+                    else
+                    {
+                        MessageBox.Show("COM Port error");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("COM Port error");
+            }
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                string data = serialPort1.ReadLine();
+                if (!string.IsNullOrEmpty(data))
+                {
+                    
+
+                    //delegateを呼び出す
+                    Invoke(new AppendTextDelegate(packet_restortion), data);
+
+                    Console.WriteLine("風速[m/s]: {0}", data);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+    //delegate（処理を委譲）
+    delegate void AppendTextDelegate(string text);
+
+    //委譲先の関数
+    private void packet_restortion(string data)
+    {
+        label3.Text = data;
+    }
+
+    private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
